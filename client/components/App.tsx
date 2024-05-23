b263ff465db1f2e1fde9891b66b5409d81026d8a
Andrew Keatinge <andrewgkeatinge@gmail.com>
2024-05-23 22:14:46 +1200

    both type components should be in a working state

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,19 @@
+import TypesList from "./Type-list"
+import Types from "./Types"
+
 function App() {
   return (
+    <>
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+      <TypesList />
+      
     </div>
+    <div>
+    <Types />
+    </div>
+    </>
   )
 }
 
--- client/components/Types.tsx
+++ client/components/Types.tsx
@@ -1,8 +1,31 @@
 import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import data from '../../data/pokedex'
+import { Pokemon } from '../../data/pokedex'
 
 function Types() {
-  return
+
+  const { name: pokemonName } = useParams()
+
+  const pokemonData = data[pokemonName || ""]
+
+  if(pokemonData) {
+    const pokemon = pokemonData.pokemons
+
+
+  return (
+    <>
+    <div>
+      <ul>
+        {pokemon.map((pokemon: Pokemon) => (
+          <li key={pokemon.code}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
+    
+    </>
+  )
+}
 }
 
 export default Types
